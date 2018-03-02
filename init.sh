151f6157f94719552eae0f2f6da0adecad7d8a49
Joan Arcas <drumsjuanma@gmail.com>
2018-03-02 11:34:38 +0100

    update prints

--- init.sh
+++ init.sh
@@ -1,17 +1,16 @@
-echo 'Creando certificados'
+echo '//////// --- Creando certificados --- ////////'
 cryptogen generate --config ./crypto-config.yaml
 
-
-echo 'creando bloque genesis'
+echo '//////// --- Creando bloque genesis --- ////////'
 export FABRIC_CFG_PATH=$PWD
 configtxgen -profile TwoOrgsOrdererGenesis -outputBlock ./channel-artifacts/genesis.block
 
-echo 'channel'
+echo '//////// --- Creando canal Caixabank-Alastria --- ////////'
 export CHANNEL_NAME=mychannel  && configtxgen -profile TwoOrgsChannel -outputCreateChannelTx ./channel-artifacts/channel.tx -channelID $CHANNEL_NAME
 
-echo 'anchor peer caixabank'
+echo '//////// --- Creando Anchor Peer Caixabank --- ////////'
 configtxgen -profile TwoOrgsChannel -outputAnchorPeersUpdate ./channel-artifacts/CaixabankMSPanchors.tx -channelID $CHANNEL_NAME -asOrg CaixabankMSP
 
-echo 'anchor peer alastria'
+echo '//////// --- Creando Anchor Peer Alastria --- ////////'
 configtxgen -profile TwoOrgsChannel -outputAnchorPeersUpdate ./channel-artifacts/AlastriaMSPanchors.tx -channelID $CHANNEL_NAME -asOrg AlastriaMSP
 
