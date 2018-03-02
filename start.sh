e73873a893f534e04f2b9082fbb541130362b59a
Joan Arcas <drumsjuanma@gmail.com>
2018-03-02 13:43:04 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,7 @@
 echo -e '\e[42m//////// --- Iniciando Red AlastriaNet --- ////////\e[49m'
 
-IMAGE_TAG="latest" CHANNEL_NAME="mychannel" COMPOSE_PROJECT_NAME="alastriaNet" docker-compose -f docker-compose-cli.yaml up -d
+export CHANNEL_NAME=mychannel
+CHANNEL_NAME=$CHANNEL_NAME docker-compose -f docker-compose-cli.yaml up
 
 echo -e '\e[92m//////// --- Exito --- ////////\e[39m'
 
